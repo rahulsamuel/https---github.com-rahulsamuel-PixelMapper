5bd249ecb47c3088d624750357b34ff9cc755412
Rahul Samuel <rahulsamuel@gmail.com>
2025-07-25 03:53:11 +0000

    make logo on homepage bigger to match the size of logo in /app page

--- src/components/landing/landing-page.tsx
+++ src/components/landing/landing-page.tsx
@@ -16,7 +16,7 @@ export async function LandingPage() {
       <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
         <div className="container flex h-14 items-center">
           <Link href="/" className="mr-6 flex items-center space-x-2">
-            <Logo className="h-6 w-6 text-primary" />
+            <Logo className="h-8 w-8 text-primary" />
             <span className="font-bold sm:inline-block">MapMyLED</span>
           </Link>
           <div className="flex flex-1 items-center justify-end space-x-2">
