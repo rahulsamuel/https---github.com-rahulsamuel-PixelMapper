73f0cd106de0bd45b8592112400d56f7a8142f3b
Rahul Samuel <63064873+rahulsamuel@users.noreply.github.com>
2026-07-03 10:58:48 -0400

    Clear .next cache for fresh build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 
-import type {Metadata} from 'next';
+import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { AuthProvider } from '@/contexts/auth-context';
